1d1a1c2b7adcf66f5f75fc44de051061ecfe896a
Jeff Raymakers <jephy@gmail.com>
2019-02-24 22:02:02 -0800

    initial commit

---
+++ jobs.js
@@ -0,0 +1,56 @@
+const fse = require('fs-extra');
+const jr = require('jr');
+const path = require('path');
+
+const httpServerPath = path.join('node_modules', '.bin', 'http-server');
+const tslintPath = path.join('node_modules', '.bin', 'tslint');
+const webpackPath = path.join('node_modules', '.bin', 'webpack');
+
+const outDir = 'out';
+const srcDir = 'src';
+
+module.exports = () => ({
+  build: {
+    needs: ['buildDev']
+  },
+  buildDev: {
+    needs: ['bundleDev', 'copy', 'lint']
+  },
+  buildProd: {
+    needs: ['bundleProd', 'copy', 'lint']
+  },
+  bundleDev: {
+    action: jr.scriptAction(webpackPath, ['--hide-modules', '--mode', 'development'], { cwd: __dirname })
+  },
+  bundleProd: {
+    action: jr.scriptAction(webpackPath, ['--hide-modules', '--mode', 'production'], { cwd: __dirname })
+  },
+  clean: {
+    action: () => fse.remove(outDir)
+  },
+  copy: {
+    needs: [/*'copyFavicon', */'copyIndex']
+  },
+  // copyFavicon: {
+  //   action: () => fse.copy(path.join(srcDir, 'favicon', 'favicon.png'), path.join(outDir, 'favicon.png'))
+  // },
+  copyIndex: {
+    action: () => fse.copy(path.join(srcDir, 'entry', 'index.html'), path.join(outDir, 'index.html'))
+  },
+  lint: {
+    action: jr.scriptAction(tslintPath, ['src/**/*.tsx'], { cwd: __dirname })
+  },
+  // publish: {
+  //   action: jr.processAction('gsutil', ['cp', '-r', 'out/**', 'gs://www.hereslookingateuclid.com'], { cwd: __dirname })
+  // },
+  // publishBundle: {
+  //   action: jr.processAction('gsutil', ['cp', 'out/bundle.js', 'gs://www.hereslookingateuclid.com/bundle.js'],
+  //     { cwd: __dirname })
+  // },
+  serve: {
+    action: jr.scriptAction(httpServerPath, ['./out', '-o'], { cwd: __dirname })
+  },
+  watch: {
+    action: jr.scriptAction(webpackPath, ['--hide-modules', '--progress', '--watch'], { cwd: __dirname })
+  },
+});
---
+++ src/app/components/App.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+// import { HashRouter } from 'react-router-dom';
+
+// import content from '../../content';
+// import { makePage } from '../../page';
+
+// import { PageRoutes } from '../routes/PageRoutes';
+
+// const rootPage = makePage(content);
+
+export class App extends React.Component<{}> {
+
+  public render(): JSX.Element {
+    return (
+      <div>Green New Deal Game</div>
+      // <HashRouter>
+      //   <PageRoutes page={rootPage} />
+      // </HashRouter>
+    );
+  }
+
+}
