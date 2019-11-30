0ffdeae5c4150a7ce3f4ab5e2b0f0adbcb6e7238
Jeff Raymakers <jephy@gmail.com>
2019-11-30 10:48:25 -0800

    define some actions

--- src/game/types/GameAction.tsx
+++ src/game/types/GameAction.tsx
@@ -1,2 +1,14 @@
-export type GameAction = {
-};
+export type ActivatePolicyGameAction = Readonly<{
+  type: 'activatePolicy'
+  action: string,
+}>;
+
+export type DeactivatePolicyGameAction = Readonly<{
+  type: 'deactivatePolicy',
+  action: string,
+}>;
+
+export type GameAction
+ = ActivatePolicyGameAction
+ | DeactivatePolicyGameAction
+ ;
